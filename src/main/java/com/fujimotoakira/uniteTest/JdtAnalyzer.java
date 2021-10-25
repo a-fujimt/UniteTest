19bf9e76b0a8096300feece2c8917f72ef5f09c0
a-fujimt <a-fujimt@ist.osaka-u.ac.jp>
2021-10-25 19:27:28 +0900

    fix static import

--- src/main/java/com/fujimotoakira/uniteTest/JdtAnalyzer.java
+++ src/main/java/com/fujimotoakira/uniteTest/JdtAnalyzer.java
@@ -2,9 +2,12 @@ package com.fujimotoakira.uniteTest;
 
 import com.fujimotoakira.uniteTest.io.FileEntity;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.dom.*;
 
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 
 public class JdtAnalyzer {
 
@@ -24,6 +27,12 @@ public class JdtAnalyzer {
 
     private CompilationUnit createTree() {
         ASTParser parser = ASTParser.newParser(AST.JLS11);
+        Hashtable<String, String> options = JavaCore.getOptions();
+        options.put(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_11);
+        options.put(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, JavaCore.VERSION_11);
+        options.put(JavaCore.COMPILER_SOURCE, JavaCore.VERSION_11);
+        options.put(JavaCore.COMPILER_DOC_COMMENT_SUPPORT, JavaCore.ENABLED);
+        parser.setCompilerOptions(options);
         parser.setSource(fileEntity.getContents().toCharArray());
         return (CompilationUnit) parser.createAST(new NullProgressMonitor());
     }
